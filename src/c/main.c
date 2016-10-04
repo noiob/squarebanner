8dab2e8bbfc0c6bd8278b3f212b247ab64229853
noiob <johannes.leuker@rwth-aachen.de>
2016-10-04 18:15:50 +0200

    Move the Bluetooth disconnect icon in the corner

--- src/c/main.c
+++ src/c/main.c
@@ -115,7 +115,7 @@ static void main_window_load(Window *window) {
   s_bt_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BT_ICON);
 
   // Create the BitmapLayer to display the GBitmap
-  s_bt_icon_layer = bitmap_layer_create(GRect(12, 12, 30, 30));
+  s_bt_icon_layer = bitmap_layer_create(GRect(0, 0, 30, 30));
   bitmap_layer_set_bitmap(s_bt_icon_layer, s_bt_icon_bitmap);
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bt_icon_layer));
   // Show the correct state of the BT connection from the start
