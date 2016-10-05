00d8b50da57a6653d2312301d335e80f2bb8272d
noiob <johannes.leuker@rwth-aachen.de>
2016-10-05 15:44:56 +0200

    Add config page via Clay (non-functional)

---
+++ src/pkjs/config.js
@@ -0,0 +1,54 @@
+module.exports = [
+  {
+    "type": "heading",
+    "defaultValue": "Square Banner Configuration"
+  },
+  {
+    "type": "section",
+    "capabilities": ["COLOR"],
+    "items": [
+      {
+        "type": "heading",
+        "defaultValue": "Banner Colors"
+      },
+      {
+        "type": "color",
+        "messageKey": "BackgroundColor",
+        "defaultValue": "0x0055AA",
+        "label": "Background Color"
+      },
+      {
+        "type": "color",
+        "messageKey": "ForegroundColor",
+        "defaultValue": "0x00AAFF",
+        "label": "Foreground Color"
+      }
+    ]
+  },
+  {
+    "type": "section",
+    
+    "items": [
+      {
+        "type": "heading",
+        "defaultValue": "Bluetooth Disconnect"
+      },
+      {
+        "type": "toggle",
+        "messageKey": "DisconnectVibrate",
+        "label": "Vibrate on Disconnect",
+        "defaultValue": true
+      },
+      {
+        "type": "toggle",
+        "messageKey": "DisconnectIcon",
+        "label": "Show Icon on Disconnect",
+        "defaultValue": true
+      }
+    ]
+  },
+  {
+    "type": "submit",
+    "defaultValue": "Save Settings"
+  }
+];
